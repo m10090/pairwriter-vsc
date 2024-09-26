89ea7e45a3c9b019b08947aa40c6647e8b3088e3
Mohamed El tamawey <tammwy22@gmail.com>
2024-09-26 16:29:04 +0300

    minor changes

--- src/pairwriter_helper/lib/load.cjs
+++ src/pairwriter_helper/lib/load.cjs
@@ -8,7 +8,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 //   https://www.npmjs.com/package/@neon-rs/cli
 module.exports = require('@neon-rs/load').proxy({
     platforms: {
-        // 'win32-x64-msvc': () => require('../platforms/win32-x64-msvc/index.node'), // windows is not working
+        'win32-x64-msvc': () => require('../platforms/win32-x64-msvc/index.node'), // windows is not working
         'darwin-x64': () => require('../platforms/darwin-x64/index.node'),
         'darwin-arm64': () => require('../platforms/darwin-arm64/index.node'),
         'linux-x64-gnu': () => require('../platforms/linux-x64-gnu/index.node'),
