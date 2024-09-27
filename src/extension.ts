0184c4864cc51c93b87a2a87ee775ffb32c9d03c
Mohamed El tamawey <tammwy22@gmail.com>
2024-09-28 02:41:02 +0300

    test: verify project setup

--- src/extension.ts
+++ src/extension.ts
@@ -3,19 +3,27 @@
 
 import * as vscode from "vscode";
 import { pairwriterStartServer } from "./server";
-
+import fs from "fs";
+import path from "path";
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activate
-  process.env.LOGFILE = context.logUri.toString() + "pairwriter.log";
+
+  fs.mkdir(context.logUri.fsPath, { recursive: true }, (err) => {
+    if (err) {
+      vscode.window.showErrorMessage(err.message);
+    }
+  });
+
+  process.env.LOGFILE = path.join(context.logUri.fsPath, "pairwriter.log");
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
   const disposable = vscode.commands.registerCommand(
     "pairwriter.startserver",
-    pairwriterStartServer
+    pairwriterStartServer,
   );
 
   context.subscriptions.push(disposable);
